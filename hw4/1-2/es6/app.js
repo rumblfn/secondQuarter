1a02cf345ce452c51bcc8f860204946982e90c9e
aakrumbl <toshamilgis@gmail.com>
2021-08-10 14:14:41 +0300

    homework4

---
+++ hw4/1-2/es6/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class Post {
+    constructor(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+
+    edit(text) {
+        this.text = text;
+    }
+}
+
+class AttachedPost extends Post {
+    constructor(author, text, date) {
+        super(author, text, date);
+        this.highlighted = false;
+    }
+
+    makeTextHighlighted() {
+        this.highlighted = true;
+    }
+}
